37706df85cf6e61b62d864c7da829edb675034dc
Ian Veloso <dev@ianveloso.com>
2020-06-05 21:42:09 -0500

    Upgrade babel and change expense formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import moment from 'moment';
+import numeral from 'numeral';
 import { Link } from 'react-router-dom';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
@@ -6,7 +8,11 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
        <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
        </Link>
-       <p>{amount}</p>
+       <p>
+        {numeral(amount / 100).format('$0,0.00')}
+        -
+        {moment(createdAt).format('MMMM Do, YYYY')}
+       </p>
        <p>{createdAt}</p>
     </div>
 );
